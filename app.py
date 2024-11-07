38ac238012cd75c36d3d2cb88a7566d298a0ad59
Nicte <167065469+Akzyn@users.noreply.github.com>
2024-11-07 02:48:40 -0600

    Se añadió botones para la app

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
+# importar las librerias
 import streamlit as st
 import pandas as pd
-import plotly_express as px
+import plotly.express as px
+
+# leer los datos
+car_data = pd.read_csv(
+    'E:/Tripleten/Sprint 6/Proyecto6/vehicles_project6/vehicles_us.csv')
+# crear un boton
+hist_button = st.button('Contruir un histograma')
+
+# configurar el boto
+if hist_button:
+    st.write(
+        'Creacion de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x='odometer')
+
+    # mostrar un grafico plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
