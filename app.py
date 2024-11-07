98ab3ec511cbf86222dbf03689eca8aeb4d2fdf0
Nicte <167065469+Akzyn@users.noreply.github.com>
2024-11-07 04:00:56 -0600

    agrego datos de grafico de dispersión

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ hist_button = st.button('Contruir un histograma')
 
 # configurar el boto
 if hist_button:
-    st.write(
-        'Creacion de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creacion de un histograma para los datos de odómetro')
 
     # crear un histograma
     fig = px.histogram(car_data, x='odometer')
@@ -19,4 +18,22 @@ if hist_button:
     # mostrar un grafico plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
+
+#Crear grafico de dispersion
+#leer los datos para grafica de dispersion
+car_data2 = pd.read_csv('vehicles_us.csv')
+
+#crear un boton
+scatter_button = st.button('Construir un gráfico de dispersión')
+
+#configurar el boton
+if scatter_button:
+    st.write('Creación de un gráfico de dispersión para datos de odómetro')
+
+    #creación del gráfico de dispersión
+    fig2 = px.scatter_3d(car_data2, x='odometer')
+
+    #mostrar el grafico en plotly interactivo
+    st.plotly_chart(fig2, use_container_width=True)
+    
 
